71c8693cb95714015f11be5e1f3b719fe3cda315
Ömer Eskizara <omereskizara@gmail.com>
2019-06-22 21:32:30 +0300

    debug updates

--- src/main.c
+++ src/main.c
@@ -24,8 +24,9 @@ int main(int argc, char *argv[])
 
     while( lirc_nextcode(&code) == 0)
     {
+        printf("\nCode Received: ", code);
         if (code == NULL) continue;
-        printf("%s\n", code);
+        printf("%s", code);
         free(code);
         if(ret==-1) break;
     }
